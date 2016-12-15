91a9bafc76287086f651410ef9e904d9051173fd
daithi2101 <daithi2101@hotmail.com>
2016-12-15 23:42:51 +0000

    Streamlined Question 4 of the 2015 christmas exam

--- src/christmas2015/Question4_2015.java
+++ src/christmas2015/Question4_2015.java
@@ -6,43 +6,33 @@ import java.util.Scanner;
 public class Question4_2015 {
     public static void main(String args[]) {
         String ppsNumber;
+        int i;
 
         Scanner input = new Scanner(System.in);
 
         System.out.print("Please enter a PPS number: ");
         ppsNumber = input.nextLine();
 
-        if(ppsNumber.length() == 8) {
-            for(int x = 0; x < (ppsNumber.length() - 1); x++) {
-                if(!(ppsNumber.charAt(x) >= '0' && ppsNumber.charAt(x) <= '9')) {
-                    System.out.print("\nInvalid PPS number. At least one of the first seven characters were not digits");
+        if(ppsNumber.length() == 8 || ppsNumber.length() ==9) {
+            for(i = 0; i < 7; i++) {
+                if(!(ppsNumber.charAt(i) >= '0' && ppsNumber.charAt(i) <= '9')){
+                    System.out.println("\nInvalid PPS number. At least one of the first seven characters were not digits");
                     break;
                 }
             }
-            if(!(ppsNumber.charAt(7) >= 'A' && ppsNumber.charAt(7) <= 'Z')) {
-                System.out.print("\nInvalid PPS number. The last or second-last character, or both, were not " +
-                        "uppercase letters");
-            } else {
-                System.out.print("\nYou entered a valid PPS number");
-            }
-        } else if(ppsNumber.length() == 9) {
-            for(int x = 0; x < (ppsNumber.length() - 2); x++) {
-                if (!(ppsNumber.charAt(x) >= '0' && ppsNumber.charAt(x) <= '9')) {
-                    System.out.print("\nInvalid PPS number. At least one of the first seven characters were not digits");
+            while(i >= 7 && i < ppsNumber.length()) {
+                if(ppsNumber.charAt(i) >= 'A' && ppsNumber.charAt(i) <= 'Z')
+                    i++;
+                else{
+                    System.out.println("\nInvalid PPS number. The last or second-last character, or both, were not " +
+                            "uppercase letters");
                     break;
                 }
             }
-            if (!((ppsNumber.charAt(7) >= 'A' && ppsNumber.charAt(7) <= 'Z') && (ppsNumber.charAt(8) >= 'A' &&
-                    ppsNumber.charAt(8) <= 'Z'))) {
-                System.out.print("\nInvalid PPS number. The last or second-last character, or both, were not " +
-                        "uppercase letters");
-            } else {
-                System.out.print("\nYou entered a valid PPS number");
-            }
-        }
-        else {
-            System.out.print("\nInvalid PPS number. It must contain 8 or 9 characters");
-        }
+            if(i == ppsNumber.length())
+                System.out.println("\nYou entered a valid PPS number");
+        } else
+            System.out.println("\nInvalid PPS number. It must contain 8 or 9 characters");
         input.close();
     }
 }
